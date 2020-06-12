0192929386a64eabd026db4908850432c8f6a59f
Ben Imberman <bimberman@gmail.com>
2020-06-12 13:13:58 -0700

    Changed the short description to show only 2 lines so that it is awlways uniform

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 export default class ProductListItem extends React.Component {
 
   render() {
-    const { name, price, shortDescription, image } = this.props;
+    const { name, price, image } = this.props;
+    let shortDescription = this.props.shortDescription;
+
+    if (shortDescription.length > 110) {
+      shortDescription = shortDescription.substr(0, 109).concat(String.fromCharCode(8230));
+    }
     return (
       <div className="container card col-3 m-3" style={{ height: '60vh' }}>
         <img
           src={image}
-          className="card-img-top img-thumbnail mt-3"
-          style={{ height: '60%', resizeMode: 'cover' }}
+          className="card-img-top img-icon img-thumbnail mt-3"
           alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{name}</h5>

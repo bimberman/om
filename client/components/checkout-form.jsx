877e7238910f3f0967507e95cc7a0e7537df47b0
Ben Imberman <bimberman@gmail.com>
2020-10-28 19:42:31 -0700

    Added back button to cart, checkbox of acknowledgement, and prevented reset of the form

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -7,11 +7,14 @@ export default class CheckoutForm extends React.Component {
     this.state = {
       name: '',
       creditCard: '',
-      shippingAddress: ''
+      shippingAddress: '',
+      checked: false
     };
     this.handleCatalogClick = this.handleCatalogClick.bind(this);
+    this.handleCartClick = this.handleCartClick.bind(this);
     this.handleSubmitClick = this.handleSubmitClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleChange(event) {
@@ -24,18 +27,32 @@ export default class CheckoutForm extends React.Component {
     });
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      checked: value
+    });
+  }
+
   handleCatalogClick() {
     this.props.setView('catalog', {});
   }
 
+  handleCartClick() {
+    this.props.setView('cart', {});
+  }
+
   handleSubmitClick(event) {
     event.preventDefault();
-    this.props.placeOrder({
-      name: this.state.name,
-      creditCard: this.state.creditCard,
-      shippingAddress: this.state.shippingAddress
-    });
-    this.resetForm();
+    if (this.state.checked) {
+      this.props.placeOrder({
+        name: this.state.name,
+        creditCard: this.state.creditCard,
+        shippingAddress: this.state.shippingAddress
+      });
+    }
   }
 
   resetForm() {
@@ -58,6 +75,19 @@ export default class CheckoutForm extends React.Component {
             href="#"
             onClick={this.handleCatalogClick}>
             &lt; Back to catalog
+          </a>
+          <a
+            type="button"
+            className="back btn btn-link border-0 mb-3 text-muted text-decoration-none"
+            href="#">
+            /
+          </a>
+          <a
+            type="button"
+            className="back btn btn-link border-0 mb-3 text-muted text-decoration-none"
+            href="#"
+            onClick={this.handleCartClick}>
+            &lt; Back to cart
           </a>
         </div>
         <h1 className="">My Cart</h1>
@@ -97,6 +127,20 @@ export default class CheckoutForm extends React.Component {
               value={shippingAddressValue}></textarea>
           </div>
           <div className="row d-flex justify-content-between m-0">
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <input
+                    type="checkbox"
+                    checked={this.state.checked}
+                    onChange={this.handleInputChange}
+                    aria-label="Checkbox for following text input" />
+                </div>
+                <div className="border d-flex">
+                  <p className="font-italic p-2 m-0">I acknowledge that this website is used for demonstration purposes only. This site will not sell any commercial product and I will not share any personal information on this site!</p>
+                </div>
+              </div>
+            </div>
             <button
               type="button"
               onClick={this.handleSubmitClick}
